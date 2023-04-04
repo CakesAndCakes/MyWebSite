bf649cc345fde90100df758740bffe7480be399e
giovani henrique <giancarlo349sub@gmail.com>
2023-04-04 20:21:34 -0300

    meu teste

---
+++ components/Header.js
@@ -0,0 +1,17 @@
+import Link from 'next/link'
+import styles from '../styles/Header.component.css'
+
+export default function Header(){
+    return(
+        <header className='HEADER'>
+            <h2 className='logo'>Logo</h2>
+            <nav className="navigation">
+                <Link className="Link"href="/">Home</Link>
+                <Link className="Link" href="/about">About</Link>
+                <Link className="Link" href="/contact">Contact</Link>
+                <Link className="Link" href="/services">None</Link>
+                <Link className="ButtonStore" href="/store">Store</Link>
+            </nav>
+        </header>
+    )
+}
